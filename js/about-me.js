49a141998d9753aff7dbc67d6864e382eec9f507
Julius Williams <doctorj89@gmail.com>
2017-02-13 19:08:30 -0500

    cleaned code up

--- js/about-me.js
+++ js/about-me.js
@@ -1,27 +1,27 @@
 var moment = require('moment'); 
 moment().format();
 
-var firstName = 'Julius';
-var lastName = 'Williams';
-var fullName = `${firstName} ${lastName}`;
-var email = 'doctorj89@gmail.com';
-var grewUpInCity = true;
-var yearsInCollege = 2;
-var numberOfPets = 1;
-var averageSleep = 7.0;
-var favoriteGenre = 'Classic Rock';
-var favoriteShows = ['Doctor Who', 'Arrested Development', 'Seinfeld'];
-var futureJobTitle;
-var lifeWithoutJS = null;
-var yearGraduated;
-var yearGraduatedMoment;
+var firstName = 'Julius'; // String
+var lastName = 'Williams'; // String
+var fullName = `${firstName} ${lastName}`; // Template 
+var email = 'jmichaelcodes@gmail.com'; // String
+var grewUpInCity = true; //Boolean
+var yearsInCollege = 2; // Number
+var numberOfPets = 1; // Number
+var averageSleep = 7.5; // Foat
+var favoriteGenre = 'Classic Rock'; // String
+var favoriteShows = ['Doctor Who', 'Arrested Development', 'Seinfeld']; // Array
+var futureJobTitle; // Undefined
+var lifeWithoutJS = null; // null
+var yearGraduated; // Date
+var yearGraduatedMoment; // Date
 
-// yearGraduated with native JS
+// Year graduated using Date object
 yearGraduated = new Date();
 yearGraduated.setFullYear(2007);
 yearGraduated = yearGraduated.getFullYear();
 
-// yearGraduated 
+// Year graduated using moment.js
 yearGraduatedMoment = moment().set('year', 2007);
 yearGraduatedMoment = yearGraduatedMoment.get('year');
 
